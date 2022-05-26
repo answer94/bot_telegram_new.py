caf9e8ed0c6e432ec3033816102f29322bd6b9f9
ad94@bk.ru <941013ADsse@>
2022-05-26 15:42:40 +0300

    26.05

--- create_bot.py
+++ create_bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import os
-TOKEN = "1178162821:AAFCT37EOFHNqBHtP1HvRMUvXjKWL9TN1H4"
+TOKEN = os.getenv("TOKEN")
 storage = MemoryStorage()
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=storage)
